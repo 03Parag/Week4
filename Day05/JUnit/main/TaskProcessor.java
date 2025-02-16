df9134ce54c4d2ecd479e5f2dd03e7be06eef93b
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-16 12:12:28 +0530

    [Parag Agrawal] Added problem of task processor that has longRunningTask method that sleeps for 3 second and check to see it fails if it takes more than 2 seconds

---
+++ Day05/JUnit/main/TaskProcessor.java
@@ -0,0 +1,15 @@
+package com.Week4.Day05.JUnit.main;
+
+// Create TaskProcessor class to make a method that sleeps for 3 sec before returning the result
+class TaskProcessor {
+    // Method to make task run long
+    public String longRunningTask() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        return "Task Completed";
+    }
+}
+
