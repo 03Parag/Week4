5495c66312d3302c5f157eba816698001a7f6e08
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-16 11:10:44 +0530

    [Parag Agrawal] Added problem of even number that has isEven method and return true if it is even and check it for many elements

---
+++ Day05/JUnit/main/EvenNumber.java
@@ -0,0 +1,10 @@
+package com.Week4.Day05.JUnit.main;
+
+// Create EvenNumber class to check if a number is even
+class EvenNumber {
+    // Method to check if a number is even
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
+
