8f430ba6675f0e8d1b386160521a74d77654eb51
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-16 11:04:18 +0530

    [Parag Agrawal] Added problem of division with JUnit test that create a divide method that throws an ArithmeticException if b is zero and verify if the exception is thrown properly

---
+++ Day05/JUnit/main/Division.java
@@ -0,0 +1,13 @@
+package com.Week4.Day05.JUnit.main;
+
+// Create Division class to divide the two number
+class Division {
+    // Method to divide
+    public int divide(int a, int b) {
+        // If the b is zero
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
+}
